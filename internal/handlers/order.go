fb68ce7ff4812629fed2603cbf306e8c220d3d6c
Thiago Ribeiro <65740833+yrnThiago@users.noreply.github.com>
2025-04-07 09:16:59 -0300

    fix: rename handlers files

---
+++ internal/handlers/order.go
@@ -0,0 +1,103 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/yrnThiago/api-server-go/internal/cmd/pub"
+	"github.com/yrnThiago/api-server-go/internal/usecase"
+)
+
+type OrderHandlers struct {
+	OrderUseCase *usecase.OrderUseCase
+}
+
+func NewOrderHandlers(
+	createOrderUseCase *usecase.OrderUseCase,
+) *OrderHandlers {
+	return &OrderHandlers{
+		OrderUseCase: createOrderUseCase,
+	}
+}
+
+func (p *OrderHandlers) Add(w http.ResponseWriter, r *http.Request) {
+	var input usecase.OrderInputDto
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	output, err := p.OrderUseCase.Add(input)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(output)
+
+	go pub.SendMessage(output)
+}
+
+func (p *OrderHandlers) GetMany(w http.ResponseWriter, r *http.Request) {
+	output, err := p.OrderUseCase.GetMany()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(output)
+}
+
+func (p *OrderHandlers) GetById(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	output, err := p.OrderUseCase.GetById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(output)
+}
+
+func (p *OrderHandlers) UpdateById(w http.ResponseWriter, r *http.Request) {
+	var input map[string]any
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	id := chi.URLParam(r, "id")
+	output, err := p.OrderUseCase.GetById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = p.OrderUseCase.UpdateById(output, input)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(output)
+}
+
+func (p *OrderHandlers) DeleteById(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	err := p.OrderUseCase.DeleteById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode("Order removed")
+}
---
+++ internal/handlers/product.go
@@ -0,0 +1,100 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/yrnThiago/api-server-go/internal/usecase"
+)
+
+type ProductHandlers struct {
+	ProductUseCase *usecase.ProductUseCase
+}
+
+func NewProductHandlers(
+	createProductUseCase *usecase.ProductUseCase,
+) *ProductHandlers {
+	return &ProductHandlers{
+		ProductUseCase: createProductUseCase,
+	}
+}
+
+func (p *ProductHandlers) Add(w http.ResponseWriter, r *http.Request) {
+	var input usecase.ProductInputDto
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	output, err := p.ProductUseCase.Add(input)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(output)
+}
+
+func (p *ProductHandlers) GetMany(w http.ResponseWriter, r *http.Request) {
+	output, err := p.ProductUseCase.GetMany()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(output)
+}
+
+func (p *ProductHandlers) GetById(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	output, err := p.ProductUseCase.GetById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(output)
+}
+
+func (p *ProductHandlers) UpdateById(w http.ResponseWriter, r *http.Request) {
+	var input usecase.ProductInputDto
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	id := chi.URLParam(r, "id")
+	_, err = p.ProductUseCase.GetById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	new, err := p.ProductUseCase.UpdateById(id, &input)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(new)
+}
+
+func (p *ProductHandlers) DeleteById(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	err := p.ProductUseCase.DeleteById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode("Product removed")
+}
