d8a998fcda015ae8cf25c41036417d640edbf02d
yrnThiago <thiagodias_123@hotmail.com>
2025-04-10 21:27:34 -0300

    refactor: move jwt to auth utils

--- internal/utils/Auth.go
+++ internal/utils/Auth.go
@@ -1,6 +1,42 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"fmt"
+	"time"
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func GenerateJWT(userID string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"username": userID,
+			"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		})
+
+	tokenString, err := token.SignedString(SECRET_KEY)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func VerifyJWT(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
+		return SECRET_KEY, nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+
+	return nil
+}
 
 func GenerateHashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
--- internal/utils/Cookie.go
+++ internal/utils/Cookie.go
@@ -3,11 +3,8 @@ package utils
 import (
 	"fmt"
 	"strings"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/golang-jwt/jwt"
-
 	"github.com/yrnThiago/api-server-go/config"
 	"go.uber.org/zap"
 )
@@ -48,36 +45,6 @@ func SetBearerCookie(c *fiber.Ctx, cookie *fiber.Cookie) (string, error) {
 	)
 	return newCookie, err
 }
-func GenerateJWT(userID string) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
-		jwt.MapClaims{
-			"username": userID,
-			"exp":      time.Now().Add(time.Hour * 24).Unix(),
-		})
-
-	tokenString, err := token.SignedString(SECRET_KEY)
-	if err != nil {
-		return "", err
-	}
-
-	return tokenString, nil
-}
-
-func VerifyJWT(tokenString string) error {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
-		return SECRET_KEY, nil
-	})
-	if err != nil {
-		return err
-	}
-
-	if !token.Valid {
-		return fmt.Errorf("invalid token")
-	}
-
-	return nil
-}
-
 func GetFormattedAuthToken(token string) (string, error) {
 	return strings.Replace(token, BEARER_KEY, "", 1), nil
 }
