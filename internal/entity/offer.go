f9177d7ee811e30f28a45e8e514563e6e0b333df
yrnThiago <thiagodias_123@hotmail.com>
2025-05-18 17:31:37 -0300

    fix: add product,buyer,seller rel

--- internal/entity/offer.go
+++ internal/entity/offer.go
@@ -17,9 +17,12 @@ type Offer struct {
 	ID        string
 	Price     float64
 	Status    OfferStatus
-	ProductID string
-	SellerID  string
-	BuyerID   string
+	ProductID string  `gorm:"index"                json:"product_id"`
+	Product   Product `gorm:"foreignKey:ProductID"`
+	SellerID  string  `gorm:"index"                json:"seller_id"`
+	Seller    User    `gorm:"foreignKey:SellerID"`
+	BuyerID   string  `gorm:"index"                json:"buyer_id"`
+	Buyer     User    `gorm:"foreignKey:BuyerID"`
 	gorm.Model
 }
 
